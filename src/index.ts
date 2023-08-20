57fd64621fb2e90fe68a49230f7c800fd1d8fa5c
VictorMFL <vm859222@gmail.com>
2023-08-20 20:44:08 -0300

    feat: add express initial server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log("hello world");
+import express from "express";
+import { config } from "dotenv";
+
+config();
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.end("Hello world")
+})
+
+const port = process.env.PORT || 8000
+
+app.listen(port, () => console.log(`Listening on port ${port}`))
