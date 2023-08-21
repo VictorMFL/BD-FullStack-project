0ab9b80606ec73207dbe3012efe701077d9825a3
VictorMFL <vm859222@gmail.com>
2023-08-21 19:56:03 -0300

    feat: add id to User

--- src/repositories/get-users/mongo-get-users.ts
+++ src/repositories/get-users/mongo-get-users.ts
@@ -1,9 +1,12 @@
 import { GetUsersRepositoryProps } from "../../controllers/get-users/protocols";
+import { MongoClient } from "../../database/mongo";
 import { User } from "../../models/user";
 
 export class MongoGetUsersRepository implements GetUsersRepositoryProps {
   async getUsers(): Promise<User[]> {
+    const users = await MongoClient.db.collection<User>('users').find({}).toArray()
     return [{
+      id: "1",
       firstName: "Victor",
       lastName: "Laurentino",
       email: "vm859222@gmail.com",
