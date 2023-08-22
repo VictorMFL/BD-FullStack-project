0a699f01163644950b795098015535bd8fa85b99
VictorMFL <vm859222@gmail.com>
2023-08-22 20:29:59 -0300

    feat: add UpdateUserController with validations

--- src/controllers/update-user/protocols.ts
+++ src/controllers/update-user/protocols.ts
@@ -1,9 +1,14 @@
 import { User } from "../../models/user";
+import { HttpRequest, HttpResponse } from "../protocols";
 
 export interface UpdateUserParams {
   firstName?: string;
   lastName?: string;
   password?: string;
+}
+
+export interface UpdateUserControllerProps {
+  handle(httpRequest: HttpRequest<any>):Promise<HttpResponse<User>>
 }
 
 export interface UpdateUserRepositoryProps {
